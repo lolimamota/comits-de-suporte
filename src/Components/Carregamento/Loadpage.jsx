810663382812ae1d510ffd9a45fcc262ac847818
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2024-06-07 02:51:31 -0300

    Estilizando o icone de carregamento/ pagina pra login

--- src/Components/Carregamento/Loadpage.jsx
+++ src/Components/Carregamento/Loadpage.jsx
@@ -1,18 +1,28 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import PageLogin from '../Login/PageLogin.jsx'
+
 //IMPORT DAS IMAGENS
 import Logo from './Images/logo_core.png'
+import ChargeIcon from './Images/chargeicon.png'
 
 const Divisor = styled.div`
 height: 100vh;
 background-color:#2EA5C2;
 display: flex;
-justify-content:center;
+justify-content: flex-start;
 align-items: center;
 flex-direction: column;
 font-size: .6rem;
-margin-bottom: 30rem;
+`;
+
+const Button = styled.button`
+width: 50px;
+height: 50px;
+`;
+
+const ImgBtn = styled.button`
 `;
 
 function Loadpage() {
@@ -20,6 +30,8 @@ function Loadpage() {
     <Divisor>
         <img src={Logo} alt="Logo que remete a um simbolo de coração" />
         <h1>Sua saúde na palma da sua mão!</h1>
+
+        <Button onClick={PageLogin}><ImgBtn src={ChargeIcon} alt="imagem circular de carregamento" /></Button>
     </Divisor>
   )
 }
---
+++ src/Components/Login/PageLogin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PageLogin() {
+  return (
+    <div>PageLogin</div>
+  )
+}
+
+export default PageLogin
